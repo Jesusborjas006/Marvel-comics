46ed226220f0fe0232cf7cb469bbc1b2dc16ff11
Jesus Borjas <jesusbor006@gmail.com>
2023-04-19 11:43:59 -0500

    Add Navbar component with logo

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+        <h1 className="logo">Marvel Comics</h1>
+      </nav>
+  )
+}
+
+export default Navbar
