5aed02e39d097d3533d57168a04402f69b232d60
Jesus Borjas <jesusbor006@gmail.com>
2023-04-19 12:04:03 -0500

    Create ComicsContainer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import ComicsContainer from "../ComicsContainer/ComicsContainer";
 import Form from "../Form/Form";
 import Navbar from "../Navbar/Navbar";
 import "./App.css";
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Form />
+      <ComicsContainer />
     </div>
   );
 }
---
+++ src/components/ComicsContainer/ComicsContainer.js
@@ -0,0 +1,11 @@
+import "./ComicsContainer.css";
+
+const ComicsContainer = () => {
+  return (
+    <div className="comics-container">
+      <h1>Comics go here!!</h1>
+    </div>
+  );
+};
+
+export default ComicsContainer;
